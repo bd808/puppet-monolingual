09fa1f1e8998f49b4645908cd1c9f12c69362a58
Bryan Davis <bd808@wikimedia.org>
2013-08-27 22:37:34 -0600

    Fix download URL.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@
 class monolingual {
   package { 'monolingual':
     ensure    => installed,
-    source    => 'http://downloads.sourceforge.net/project/monolingual/monolingual/1.5.7/Monolingual-1.5.7.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmonolingual%2Ffiles%2Fmonolingual%2F&ts=1377642621&use_mirror=softlayer-dal',
+    source    => 'http://hivelocity.dl.sourceforge.net/project/monolingual/monolingual/1.5.7/Monolingual-1.5.7.dmg',
     provider  => appdmg,
   }
 }
